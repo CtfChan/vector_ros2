a0d747ab0df5ded2e1f0c2d1490168a35cd301c0
CtfChan <christopherchan@pop-os.localdomain>
2019-04-14 14:55:07 -0400

    adding init files, can say hello world now

---
+++ README.md
@@ -0,0 +1,13 @@
+# ROS2 Driver for Anki Vector 
+
+
+
+
+<!-- ```console
+$ mkdir -p ~/ros2_ws/src && cd ~/ros2_ws/src
+$ git clone https://github.com/CtfChan/ros2_template_py.git
+
+$ colcon build --symlink-install
+$ source ~/ros2_ws/install/setup.bash && source ~/ros2_ws/install/local_setup.bash
+$ ros2 run ros2_template_py demo
+``` -->
---
+++ setup.py
@@ -0,0 +1,30 @@
+from setuptools import setup
+
+package_name = 'vector_ros2'
+
+setup(
+    name=package_name,
+    version='0.0.0',
+    packages=[],
+    py_modules=[],
+    install_requires=['setuptools'],
+    author='Esteve Fernandez',
+    author_email='esteve@osrfoundation.org',
+    maintainer='Esteve Fernandez',
+    maintainer_email='esteve@osrfoundation.org',
+    keywords=['ROS'],
+    classifiers=[
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: Apache Software License',
+        'Programming Language :: Python',
+        'Topic :: Software Development',
+    ],
+    description='Package containing examples of how to use the rclpy API.',
+    license='Apache License, Version 2.0',
+    test_suite='test',
+    entry_points={
+        'console_scripts': [
+            'vision = scripts.vision:main'
+        ],
+    },
+)
